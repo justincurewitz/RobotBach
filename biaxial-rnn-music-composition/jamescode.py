25a09c9b202908a8b1f85ce1f40aac0f3652702f
Justin Curewitz <Justin Curewitz>
2017-04-28 16:23:42 -0500

    added run time

--- biaxial-rnn-music-composition/jamescode.py
+++ biaxial-rnn-music-composition/jamescode.py
@@ -1,11 +1,17 @@
 import model
+import time
 m = model.Model([300,300],[100,50],dropout=0.5)
 
 import multi_training
 pcs = multi_training.loadPieces("music")
 
 print "Loaded Pieces"
-
+start = time.time();
 multi_training.trainPiece(m, pcs, 10000)
-
+end = time.time();
 print "Trained"
+total = end-start
+hours = total/3600
+minutes = (total%3600)/60
+seconds = (total%60)
+print 'Elapsed time: %i:%i:%i' %(hours, minutes, seconds)
