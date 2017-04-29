e4d9688a3f9961d99daf26ba25eb0741b00cdebf
Justin Curewitz <curewitzjustin@gmail.com>
2017-04-29 10:21:03 -0500

    modified neural net

--- biaxial-rnn-music-composition/jamescode.py
+++ biaxial-rnn-music-composition/jamescode.py
@@ -1,14 +1,16 @@
 import model
 import time
-m = model.Model([300,300],[100,50],dropout=0.5)
+m = model.Model([30,30],[10,5],dropout=0.5)
 
 import multi_training
 pcs = multi_training.loadPieces("music")
 
 print "Loaded Pieces"
+
 start = time.time();
 multi_training.trainPiece(m, pcs, 10000)
 end = time.time();
+
 print "Trained"
 total = end-start
 hours = total/3600
